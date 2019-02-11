4b3fe092b6e59d18a31157d09f97614121a920c8
Unknown <donaghey-b@ulster.ac.uk>
2019-02-11 17:39:13 +0000

    Map dynamically working with two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as mapquest from 'mapquest';
+import { Subject, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Traffic Congestion';
+
+  // initialise variables for map, key and locations
+  baseUrl = 'https://open.mapquestapi.com/staticmap/v5/map?';
+  key = 'eGjnmBVKciAGwk5W721NKLqNRL6J73Jy';
+  locationA = '';
+  locationB = '';
+
+  // setting locations to subject observer in string arrays
+  locationASubject = new Subject<string>();
+  locationBSubject = new Subject<string>();
+
+  // setting locations to observables
+  locationA$ = this.locationASubject.asObservable();
+  locationB$ = this.locationBSubject.asObservable();
+
+  // placeholder map
+  mapUrl =
+    'https://open.mapquestapi.com/staticmap/v5/map?key=eGjnmBVKciAGwk5W721NKLqNRL6J73Jy&center=Derry&size=1000,700';
+
+  constructor() {
+    // combines both locations lat and long to mapURL string with key
+    combineLatest(this.locationA$, this.locationB$).subscribe((res: any) => {
+      this.mapUrl = `${this.baseUrl}start=${res[0].lat},${
+        res[0].lng
+      }%7Cflag-start&end=${res[1].lat},${res[1].lng}%7Cflag-end&size=@2x&key=${
+        this.key
+      }`;
+    });
+  }
+
+  public generateMap() {
+    // generates for user when button is clicked
+    mapquest.geocode(
+      { address: this.locationA, key: this.key },
+      (err, location) => {
+        console.log(location);
+        this.locationASubject.next(location.latLng);
+      }
+    );
+
+    mapquest.geocode(
+      { address: this.locationB, key: this.key },
+      (err, location) => {
+        this.locationBSubject.next(location.latLng);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MatInputModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
